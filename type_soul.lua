d5c6a520605edad8a2a42f3502d6a41be3b6f287
spyedd1 <jamiegallagher0607@gmail.com>
2026-05-07 15:36:10 +0100

    aggen

--- type_soul.lua
+++ type_soul.lua
@@ -8,7 +8,7 @@ getgenv().luaguardvars = {
     DiscordName = "cashfears",
 }
 
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bothimee/testsoul/main/source"))()
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bothimee/testsoul/main/source.lua"))()
 library:init()
 
 local Window = library.NewWindow({
